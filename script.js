1338790fde5d9a3687bd36154ad0befd4b512de7
Vishnu Kumar <vishnu.kumar@brainlabsdigital.com>
2023-08-16 01:08:52 +0530

    first commit

--- script.js
+++ script.js
@@ -19,36 +19,36 @@ function showSuccess(input) {
 // Check required fields
 function checkRequired(inputArr) {
   let isRequired = false;
-  inputArr.forEach(function(input) {
-    let field_value = document.getElementById(input); 
+  inputArr.forEach(function (input) {
+    let field_value = document.getElementById(input);
     if (field_value.value.trim() === '') {
-        if (input!="lastname"){
-          document.getElementById(input).placeholder = "";
-          showError(field_value, `${getFieldName(field_value)} can not be empty`);
-          isRequired = true;
-        }
-      }  
-     else {
-      if (input=="firstname" || input=="lastname" || input=="password"){
+      if (input != "lastname") {
+        document.getElementById(input).placeholder = "";
+        showError(field_value, `${getFieldName(field_value)} can not be empty`);
+        isRequired = true;
+      }
+    }
+    else {
+      if (input == "firstname" || input == "lastname" || input == "password") {
         if (field_value.value.length < 3) {
           showError(field_value, `${getFieldName(field_value)} can not be less than 3 letters`);
         }
-        else if (field_value.value.length > 13){
+        else if (field_value.value.length > 13) {
           showError(field_value, `${getFieldName(field_value)} can not be greater than 13`);
         }
-        else{
+        else {
           let letters = /^[A-Za-z]+$/;
-          if (input=="firstname" || input=="lastname"){
+          if (input == "firstname" || input == "lastname") {
             if (letters.test(field_value.value.trim())) {
               showSuccess(field_value);
             } else {
               showError(field_value, 'Enter only aplhabets');
             }
           }
-          
-        } 
-      }  
-      else if (input=="email"){
+
+        }
+      }
+      else if (input == "email") {
         const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if (re.test(field_value.value.trim())) {
           showSuccess(field_value);
@@ -68,7 +68,30 @@ function getFieldName(input) {
 }
 
 // Event listeners
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
   e.preventDefault();
-  checkRequired(["firstname", "lastname", "email", "password"]) ;
+  checkRequired(["firstname", "lastname", "email", "password"]);
 });
+
+
+function submitForm() {
+  var phoneNumber = document.getElementById('number').value;
+
+  var access_key = 'e0fdef3e83ea1f7bb7cdd3840f88a29c'; // Replace with your API access key
+
+  fetch('http://apilayer.net/api/validate?access_key=' + access_key + '&number=' + phoneNumber)
+    .then(response => response.json())
+    .then(json => {
+      // Access and use your preferred validation result objects
+      var validationResult = '';
+      validationResult += 'Phone Number: ' + phoneNumber + '<br>';
+      validationResult += 'Valid: ' + json.valid + '<br>';
+      validationResult += 'Country Code: ' + json.country_code + '<br>';
+      validationResult += 'Carrier: ' + json.carrier + '<br>';
+
+      document.getElementById('validationResult').innerHTML = validationResult;
+    })
+    .catch(error => {
+      document.getElementById('validationResult').innerHTML = 'Error occurred while validating the phone number.';
+    });
+}
